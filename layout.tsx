93c46fec863f1f693eb5fa5053e81e61081ab3d1
millielynnash <149213694+millielynnash@users.noreply.github.com>
2025-03-26 08:25:10 -0400

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,10 @@
+// layout.tsx in the same folder as page.tsx
+
+export const metadata = {
+  title: '...',
+  description: '...',
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return children
+}
